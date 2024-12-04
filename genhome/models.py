d6b8730737ea9415aadb4ae4bffef5e461d60825
TitouanCh <titouan85000@gmail.com>
2024-12-04 12:24:51 +0100

    Schéma UML

--- genhome/models.py
+++ genhome/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(max_length=254, primary_key=True)
+    username = models.CharField(max_length=32)
+    pwd = models.CharField(max_length=128)
+    bio = models.CharField(max_length=1024)
+    avatar = models.ImageField(upload_to='profile_images')
+    status = models.TextChoices(
+            "Guest", "User", "Admin"
+    )
+
+class FaSequence(models.Model):
+    status = models.TextChoices(
+            "Full Genome", "Coding DNA Sequence", "Peptide Sequence"
+    )
+    sequence = models.TextField()
+

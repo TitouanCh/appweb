1e021faedb5fd19462543344db8f74f512eddbf0
MaximeParizot <104005385+MaximeParizot@users.noreply.github.com>
2025-01-07 00:08:49 +0100

    Ajout fichier par fichier

--- genhome/forms.py
+++ genhome/forms.py
@@ -4,8 +4,9 @@ from .models import FaSequence
 class FaSequenceForm(forms.ModelForm):
     class Meta:
         model = FaSequence
-        fields = ['sequence','status']  #Champs obligatoire a sauvegarder 
+        fields = ['status']  # Ajouter un champ pour le fichier
         widgets = {
             'status': forms.Select(attrs={'class': 'form-control'}),
-            'sequence': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
         }
+        # Champ pour télécharger un fichier FASTA
+    sequence_file = forms.FileField(label="Ou téléchargez votre fichier FASTA", required=True)
--- genhome/views.py
+++ genhome/views.py
@@ -5,6 +5,7 @@ from .models import FaSequence, Annotation
 from django.urls import reverse
 from .forms import FaSequenceForm
 from django.contrib import messages
+from django.core.exceptions import ValidationError
 
 def home(request):
     #homepage avec toute les sequences 
@@ -36,16 +37,26 @@ def delete_annotation(request, annotation_id):
 
 def add_sequence(request):
     if request.method == 'POST':
-        form = FaSequenceForm(request.POST)
-        #vérification si la séquence valide
-        seq = request.POST.get('sequence')  
-        if not is_dna(seq):  
-            messages.error(request, "Veuillez rentrez une sequence ADN valide")
-            return render(request, 'genhome/add_sequence.html', {'form': form})
+        form = FaSequenceForm(request.POST, request.FILES)
+        if not form.is_valid():
+            print(form.errors)  # Affichez les erreurs dans la console
         # Si oui enregistre la nouvelle sequence avec un nouvel id 
         if form.is_valid():
-            fa_sequence = form.save()  
-            return redirect('annotate_sequence', sequence_id=fa_sequence.id)
+            fasta_file = request.FILES['sequence_file']
+            sequence = extract_sequence_from_fasta(fasta_file)
+                    #vérification si la séquence valide
+            if not is_dna(sequence):  
+                messages.error(request, "Veuillez rentrez une sequence ADN valide")
+                return render(request, 'genhome/add_sequence.html', {'form': form})
+        # Si oui enregistre la nouvelle sequence avec un nouvel id 
+            # Enregistrer la nouvelle séquence dans la base de données
+            print('c')
+            new_sequence = FaSequence(
+                status=form.cleaned_data['status'],
+                sequence=sequence
+            )
+            fa_sequence = new_sequence.save() 
+            return redirect('annotate_sequence', sequence_id=new_sequence.id)
     else:
         form = FaSequenceForm()
     return render(request, 'genhome/add_sequence.html', {'form': form})
@@ -56,3 +67,19 @@ def is_dna(seq):
         if nucleotide not in ['A', 'T', 'C', 'G']:
             return False
     return True
+
+
+def extract_sequence_from_fasta(file):
+    """Extrait la séquence d'un fichier FASTA."""
+    try:
+        # Lire le contenu du fichier FASTA
+        content = file.read().decode('utf-8')
+        
+        # Séparer par les lignes et ignorer les lignes qui commencent par '>'
+        lines = content.splitlines()
+        print(lines)
+        sequence = ''.join(line for line in lines if not line.startswith('>'))
+        print(sequence)
+        return sequence
+    except Exception as e:
+        raise ValidationError(f"Erreur lors de la lecture du fichier FASTA : {e}")

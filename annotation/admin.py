1a3dfc1f99a9baa0228087d77f286c928d8c105c
MaximeParizot <104005385+MaximeParizot@users.noreply.github.com>
2025-01-15 21:17:19 +0100

    Basculement vers annotation

--- annotation/admin.py
+++ annotation/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Annotation
+from genhome.models import FaSequence
 # Register your models here.
+@admin.register(Annotation)
+class AnnotationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sequence', 'owner', 'created_at')
+    list_filter = ('sequence', 'owner', 'created_at')
+    search_fields = ('content',)
--- genhome/models.py
+++ genhome/models.py
@@ -20,21 +20,3 @@ class FaSequence(models.Model):
     def __str__(self):
         return f"Statue :{self.status}, Owner: {self.owner.email}, Sequence: {self.sequence[:30]}...,id: {self.id},"
 
-
-class Annotation(models.Model):
-    sequence = models.ForeignKey(
-        'FaSequence', 
-        on_delete=models.CASCADE, 
-        related_name='annotations'
-    )
-    content = models.TextField()  # Le texte de l'annotation
-    created_at = models.DateTimeField(auto_now_add=True)  # Date de création
-
-    owner = models.ForeignKey(
-        'authentication.BioinfoUser',
-        on_delete=models.CASCADE,
-        related_name='annotations',
-        default=1 # TODO: changer
-    )
-    def __str__(self):
-        return f"Annotation for Sequence ID {self.sequence.id}, Owner: {self.owner.email}"

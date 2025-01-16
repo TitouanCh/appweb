57b462ecd68631234ce50c79aba5219a211d19aa
Nathan Carre <nathan.carre@universite-paris-saclay.fr>
2025-01-16 11:32:57 +0100

    Test and url fixed

--- blast_request/urls.py
+++ blast_request/urls.py
@@ -4,3 +4,5 @@ from . import views
 urlpatterns = [
     path('databases/', views.database_list, name='database_list'),
 ]
+
+#go directly to 127.0.0.1:8000/blast_request/databases/ because the page blast_request/ only doesn't coded for the moment

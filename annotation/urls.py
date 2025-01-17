f61ca2d6369c9eccfebe1ae7bf346bba29ad59b6
MaximeParizot <104005385+MaximeParizot@users.noreply.github.com>
2025-01-17 14:38:26 +0100

    ajout telechargement données

--- annotation/urls.py
+++ annotation/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('view/<int:sequence_id>/', views.simple_view, name='simple_view'),
     path('annotation/delete/<int:annotation_id>/', views.delete_annotation, name='delete_annotation'),
     path('add-sequence/', views.add_sequence, name='add_sequence'),
+    path('sequence/<int:sequence_id>/download_with_annotations/', views.download_sequence_with_annotations, name='download_sequence_with_annotations'),
 ]

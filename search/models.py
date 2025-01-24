eb07a2edc94db4daa6ed54b81eb8704d1f94d1eb
Nathan Carre <nathan.carre@universite-paris-saclay.fr>
2025-01-24 21:43:19 +0100

    Delete Dead Classes

--- search/models.py
+++ search/models.py
@@ -1,13 +1,2 @@
 from django.db import models
-
-class Genome(models.Model):
-    name = models.CharField(max_length=255)
-    sequence = models.TextField()
-    species = models.CharField(max_length=255)
-
-class GeneProtein(models.Model):
-    name = models.CharField(max_length=255)
-    genome = models.ForeignKey(Genome, on_delete=models.CASCADE, related_name="genes_proteins")
-    description = models.TextField()
-    sequence = models.TextField()
-
+#Classses mortes enlevées
